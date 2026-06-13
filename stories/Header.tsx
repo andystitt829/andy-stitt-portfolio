3e9a4dc981b01e9891740e63c48c9a7c93f73055
Andy Stitt <andystitt829@gmail.com>
2026-06-12 23:08:52 -0400

    Header work

--- stories/Header.tsx
+++ stories/Header.tsx
@@ -1,11 +1,24 @@
-import { Container, Flex, Link, Spacer } from "@chakra-ui/react"
+import { Box, Container, Flex, Link, Spacer } from "@chakra-ui/react"
 
 export const Header = () => (
     <Container p="8">
-        <Flex>
+        <Flex as="nav" flexDirection={{ base: "column", md: "row"}} alignItems={{ base: "center", md: "start" }} gap="2">
             <Link href="/">Andy Stitt</Link>
             <Spacer />
-            <Link href="/about/">About</Link>
+            <Flex as="ul" gap="4">
+                <Box as="li">
+                    <Link href="/about/">About</Link>
+                </Box>
+                <Box as="li">
+                    <Link href="/work/">Work</Link>
+                </Box>
+                <Box as="li">
+                    <Link href="/services/">Services</Link>
+                </Box>
+                <Box as="li">
+                    <Link href="/services/">Contact</Link>
+                </Box>
+            </Flex>
         </Flex>
     </Container>
 );
